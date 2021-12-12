885969bcb2e556c745bb28fee46c5e0e380c034a
Naveler <georg.vilja@khk.ee>
2021-12-12 16:00:36 +0200

    midagi

---
+++ README.md
@@ -0,0 +1,3 @@
+in .env REACT_APP_API_KEY="your key"
+
+"npm start" to start
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [result, setResult] = useState("");
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLongitude(position.coords.longitude);
+      setLatitude(position.coords.latitude);
+      const url =
+        "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+        position.coords.latitude +
+        "&lon=" +
+        position.coords.longitude +
+        "&appid=" +
+        process.env.REACT_APP_API_KEY;
+      fetch(url)
+      .then(response=>response.text())
+      .then(data => (setResult(data)));
+    });
+  }, []);
+  return (
+    <div className="App">
+      <div>long: {longitude}</div>
+      <div>lat: {latitude}</div>
+      <p>result: {result}</p>
+    </div>
+  );
+}
+
+export default App;
